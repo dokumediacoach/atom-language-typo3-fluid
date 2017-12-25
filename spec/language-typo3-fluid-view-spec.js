6c1b963029fb2b90fc0aeeca4669213e5350104e
Matthias Schleiff <info@dokumediacoach.de>
2017-12-25 18:53:21 +0100

    removed useless view spec

--- spec/language-typo3-fluid-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageTypo3FluidView from '../lib/language-typo3-fluid-view';
-
-describe('LanguageTypo3FluidView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
